2dce450d6b11e374ad3d8e6e5df49b03bbfc5924
Andrew Smith <andrew@clevercode.net>
2014-08-11 19:10:44 -0400

    CRUD for Dives. Date math!

--- app/controllers/dives_controller.rb
+++ app/controllers/dives_controller.rb
@@ -2,11 +2,55 @@ class DivesController < ApplicationController
 
   before_filter :authenticate_diver!
 
+  respond_to :html
+
   def index
     @dives = current_diver.dives
   end
 
+  def show
+    @dive = Dive.find(params[:id])
+  end
+
   def new
     @dive = current_diver.dives.new
+  end
+
+  def create
+    @dive = current_diver.dives.new(dive_params)
+
+    if @dive.save
+      respond_with @dive
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dive = Dive.find(params[:id])
+
+    unless (@dive.diver == current_diver)
+      head 403
+    end
+  end
+
+  def update
+    @dive = Dive.find(params[:id])
+
+    unless (@dive.diver == current_diver)
+      head 403
+    end
+
+    if @dive.update_attributes(dive_params)
+      respond_with @dive
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def dive_params
+    params.require(:dive).permit!
   end
 end
--- app/models/dive.rb
+++ app/models/dive.rb
@@ -2,6 +2,33 @@ class Dive < ActiveRecord::Base
 
   belongs_to :diver
 
-  attr_accessor :dive_site, :began_at_time, :began_at_date
+  attr_accessor :dive_site
+
+
+  def began_at_time
+    self.began_at
+  end
+
+  def began_at_date
+    self.began_at
+  end
+
+  def began_at_time=(time)
+    time = DateTime.parse(time)
+    if (self.began_at)
+      self.began_at = self.began_at.change(:hour => time.hour, :min => time.min)
+    else
+      self.began_at = time
+    end
+  end
+
+  def began_at_date=(date)
+    date = DateTime.parse(date)
+    if (self.began_at)
+      self.began_at = self.began_at.change(:year => date.year, :day => date.day, :month => date.month)
+    else
+      self.began_at = date
+    end
+  end
 
 end
